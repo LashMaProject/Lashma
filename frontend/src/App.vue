c9183c4d0aeb3e6251a166db88f0f3ccfd51d366
Nguyễn Võ Chí Dũng <bbiacm007@gmail.com>
2024-07-03 14:35:23 +0700

    fmodi: Change directories

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,26 @@
 <template>
-  <TheNavigation />
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12">
-        <div>
-          <router-view></router-view>
-        </div>
-      </div>
-    </div>
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import TheNavigation from "./components/TheNavigation.vue";
+import HelloWorld from './components/HelloWorld.vue'
+
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    TheNavigation,
-  },
-};
+    HelloWorld
+  }
+}
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
