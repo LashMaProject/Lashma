2475920578185d9cb9147075f6bff92a4880035b
Nguyễn Võ Chí Dũng <bbiacm007@gmail.com>
2024-08-09 00:13:15 +0700

    feat: Update classify api

--- api/app.py
+++ api/app.py
@@ -3,44 +3,66 @@ from flask_jwt_extended import create_access_token,get_jwt,get_jwt_identity, uns
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 
+
 import json
 import os
+
 
 from datetime import datetime, timedelta, timezone
 from werkzeug.utils import secure_filename
 from ultralytics import YOLO
 from models import db, User
 
+
 import cv2
 
  
 api = Flask(__name__)
 CORS(api, supports_credentials=True)
- 
+
+
+UPLOAD_FOLDER = 'static/uploads'
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png'}
+
+
 api.config['SECRET_KEY'] = 'cairocoders-ednalan'
 api.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flaskdb.db'
- 
+
+
 api.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
 jwt = JWTManager(api)
- 
+
+
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = True
- 
+
+
 bcrypt = Bcrypt(api)    
 db.init_app(api)
-  
+
+
 with api.app_context():
     db.create_all()
- 
+
+
+api.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+model = YOLO("model/final.pt")
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
 @api.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
- 
+
+
 @api.route('/logintoken', methods=["POST"])
 def create_token():
     email = request.json.get("email", None)
     password = request.json.get("password", None)
-  
+
     user = User.query.filter_by(email=email).first()
     #if email != "test" or password != "test":
     #    return {"msg": "Wrong email or password"}, 401
@@ -58,7 +80,8 @@ def create_token():
         "access_token": access_token
     })
     #return response
- 
+
+
 @api.route("/signup", methods=["POST"])
 def signup():
     email = request.json["email"]
@@ -78,7 +101,8 @@ def signup():
         "id": new_user.id,
         "email": new_user.email
     })
- 
+
+
 @api.after_request
 def refresh_expiring_jwts(response):
     try:
@@ -95,13 +119,15 @@ def refresh_expiring_jwts(response):
     except (RuntimeError, KeyError):
         # Case where there is not a valid JWT. Just return the original respone
         return response
- 
+
+
 @api.route("/logout", methods=["POST"])
 def logout():
     response = jsonify({"msg": "logout successful"})
     unset_jwt_cookies(response)
     return response
- 
+
+
 @api.route('/profile/<getemail>')
 @jwt_required() 
 def my_profile(getemail):
@@ -119,3 +145,44 @@ def my_profile(getemail):
     }
   
     return response_body
+
+@api.route('/upload_image', methods=['POST'])
+def upload_image():
+    if 'image' not in request.files:
+        return jsonify({"error": "No file part"}), 400
+    file = request.files['image']
+    
+    if file.filename == '':
+        return jsonify({"error": "No selected file"}), 400
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(api.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+        image = cv2.imread(file_path)
+        results = model(image, conf=0.6
+                        , verbose=False)
+        highest_confidence = 0
+        best_class_name = ""
+        for r in results:
+            for idx, class_idx in enumerate(r.probs.top5):
+                class_name = r.names[class_idx]
+                confidence = float(r.probs.top5conf[idx])
+                if confidence > highest_confidence:
+                    highest_confidence = confidence
+                    best_class_name = class_name
+        response_body = {
+            "best_class_name": best_class_name,
+            "highest_confidence": highest_confidence
+        }
+        return jsonify({
+            "best_class_name": best_class_name,
+            "highest_confidence": highest_confidence
+        }), 200
+    else:
+        return jsonify({"error": "Invalid file type"}), 400
+
+
+if __name__ == '__main__':
+    if not os.path.exists(api.config['UPLOAD_FOLDER']):
+        os.makedirs(api.config['UPLOAD_FOLDER'])
+    api.run(debug=True)
